430bf0039587d21b26b9ddd20e94575a37c3f6b1
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-02-12 11:01:04 +0900

    Implements SectioList and its rendered items

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { View, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Image,
+  Text,
+} from "react-native";
+import { ImageProps } from "react-native-elements";
 
 type ProductDataProps = {
   title: string;
   description: string;
-  thumbnail: string;
+  thumbnail: ImageProps;
 };
 
 type ProductProps = TouchableOpacityProps & {
@@ -13,6 +20,16 @@ type ProductProps = TouchableOpacityProps & {
 
 export function Product({ data, ...rest }: ProductProps) {
   return (
-    <TouchableOpacity className="w-full flex-row items-center pb-4"></TouchableOpacity>
+    <TouchableOpacity className="w-full flex-row items-center pb-4">
+      <Image className="w-20 h-20 rounded-md" source={data.thumbnail} />
+      <View className="flex-1 ml-3">
+        <Text className="text-slate-100 font-subtitle text-base flex-1">
+          {data.title}
+        </Text>
+        <Text className="text-slate-400 text-xs leading-5 mt-0.5">
+          {data.description}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 }
